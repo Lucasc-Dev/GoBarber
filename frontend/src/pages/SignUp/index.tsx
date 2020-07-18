b64292aa5618e28259811951ba7bb5b4fd0654b3
Lucas César <lucasambrogi1@hotmail.com>
2020-07-17 23:46:41 -0300

    SignUp page created.

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -10,26 +10,30 @@ import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => (
     <Container>
+        <Background />
         <Content>
             <img src={logoImg} alt="GoBarber"/>
 
             <form>  
-                <h1>Faça seu logon</h1>
+                <h1>Faça seu cadastro</h1>
 
+                <Input name="name" icon={FiUser} placeholder="Nome" />
                 <Input name="email" icon={FiMail} placeholder="E-mail" />
-                <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+                <Input 
+                    name="email" 
+                    icon={FiLock}
+                    type="password" 
+                    placeholder="Senha" 
+                />
 
-                <Button type="submit">Entrar</Button>
-
-                <a href="forgot">Esqueci minha senha</a>
+                <Button type="submit">Cadastrar</Button>
             </form>
 
             <a href="/">
-                <FiLogIn />
-                Criar conta
+                <FiArrowLeft />
+                Voltar para logon
             </a>
         </Content>
-        <Background />
     </Container>
 );
 
--- frontend/src/pages/SignUp/styles.ts
+++ frontend/src/pages/SignUp/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import signInBackgroundImg from '../../assets/sign-in-background.png';
+import signUpBackgroundImg from '../../assets/sign-up-background.png';
 
 export const Container = styled.div`
     height: 100vh;
@@ -60,7 +60,7 @@ export const Content = styled.div`
     }
 
     > a {
-        color: #FF9000;
+        color: #F4EDE8;
         display: block;
         margin-top: 24px;
         text-decoration: none;
@@ -74,13 +74,13 @@ export const Content = styled.div`
         }
 
         &:hover {
-                color: ${shade(0.2, '#ff9000')};
+                color: ${shade(0.2, '#f4ede8')};
         }
     }
 `;
 
 export const Background = styled.div`
     flex: 1;
-    background: url(${signInBackgroundImg}) no-repeat center;
+    background: url(${signUpBackgroundImg}) no-repeat center;
     background-size: cover;
 `;
