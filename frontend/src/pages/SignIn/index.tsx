af140ebf7743c61f4608ab49d913ac91dab59152
Lucas César <lucasambrogi1@hotmail.com>
2020-07-19 21:24:04 -0300

    Added axios and auth context api

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
@@ -6,6 +6,7 @@ import * as Yup from 'yup';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
+import { AuthContext } from '../../context/AuthContext';
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -13,10 +14,17 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+    email: string;
+    password: string;
+}
+
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
-    const handleSubmit = useCallback(async (data: object) => {
+    const { signIn } = useContext(AuthContext);
+
+    const handleSubmit = useCallback(async (data: SignInFormData) => {
         try {
             formRef.current?.setErrors({});
 
@@ -28,11 +36,16 @@ const SignIn: React.FC = () => {
             await schema.validate(data, {
                 abortEarly: false,
             });
+
+            signIn({
+                email: data.email,
+                password: data.password,
+            });
         } catch (err) {
             const errors = getValidationErrors(err);
             formRef.current?.setErrors(errors);
         }   
-    }, []);
+    }, [signIn]);
 
     return (
         <Container>
