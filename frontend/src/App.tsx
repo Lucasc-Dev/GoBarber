dc5ff1d9cd10ee12b63f25b337a399bb17bdf667
Lucas César <lucasambrogi1@hotmail.com>
2020-07-20 18:09:10 -0300

    Added Routes and an animation to the SignIn and SignUp page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import SignIn from './pages/SignIn';
-//import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 import AppProvider from './hooks';
 
-const App: React.FC = () => (
-  <>
-    <AppProvider>
-      <SignIn />
-    </AppProvider>
+import Routes from './routes';
 
+const App: React.FC = () => (
+  <Router>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
     <GlobalStyle />
-  </>
+  </Router>
 );
 
 export default App;
