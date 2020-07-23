61538ead30f7228506de4840bf0e02a74d7eac94
Lucas César <lucasambrogi1@hotmail.com>
2020-07-22 23:54:11 -0300

    Added RobotoSlab fonts and started the SignIn page.

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
     return (
-        <Container />
+        <Container>
+            <Image source={logoImg} />
+
+            <Title>Faça seu logon</Title>
+        </Container>
     );
 };
 
