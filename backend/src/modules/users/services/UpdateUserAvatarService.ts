7e6ca74a254a6c01a0265e163b6c25b372123261
Lucas César <lucasambrogi1@hotmail.com>
2020-07-27 23:50:39 -0300

    Imports updated.

--- backend/src/modules/users/services/UpdateUserAvatarService.ts
+++ backend/src/modules/users/services/UpdateUserAvatarService.ts
@@ -2,10 +2,10 @@ import { getRepository } from "typeorm";
 import path from 'path';
 import fs from 'fs';
 
-import AppError from '../errors/AppError';
+import AppError from '../../../shared/errors/AppError';
 
-import User from "../models/User";
-import uploadConfig from "../config/upload";
+import User from "../entities/User";
+import uploadConfig from "../../../config/upload";
 
 interface Request{
     user_id: string;
