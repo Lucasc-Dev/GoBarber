fa14153f22e2a4bbcd4479428e1c4d0ccb88078f
Lucas César <lucasambrogi1@hotmail.com>
2020-08-01 23:40:40 -0300

    Created first automated test.

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,8 +1,20 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-    it('should be able to create a new appointment.', () => {
-        
+    it('should be able to create a new appointment.', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointment = await createAppointment.execute({ 
+            provider_id: '1234',
+            date: new Date(),
+        });
+
+        expect(appointment).toHaveProperty('id');
+        expect(appointment.provider_id).toBe('1234');
     });
 
     /* it('should not be able to create two appointments at the same time.', () => {
