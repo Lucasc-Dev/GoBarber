b5af2981609a35ef2a6f9eca8eec78e77244006f
Lucas César <lucasambrogi1@hotmail.com>
2020-08-11 23:55:02 -0300

    Created FakeUserTokensRepository.

---
+++ backend/src/modules/users/repositories/IUserTokensRepository.ts
@@ -0,0 +1,6 @@
+import User from "../infra/typeorm/entities/User";
+import UserToken from "../infra/typeorm/entities/UserToken";
+
+export default interface IUserTokensRepository {
+    generate(user_id: string): Promise<UserToken>;
+}
