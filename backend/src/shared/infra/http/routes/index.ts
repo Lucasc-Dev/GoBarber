0772dc287676d1552280cfd14b35fac84b672038
Lucas César <lucasambrogi1@hotmail.com>
2020-08-15 23:49:53 -0300

    Created profile routes.

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes'
+import profileRouter from '@modules/users/infra/http/routes/profile.routes'
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 
 const routes = Router();
@@ -11,5 +12,6 @@ routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
