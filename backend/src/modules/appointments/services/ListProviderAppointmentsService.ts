086af7edf74d344ca8cb07fece55780daca570aa
Lucas César <lucasambrogi1@hotmail.com>
2020-08-22 01:55:56 -0300

    Added a limit of connections from a single ip.

--- backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -36,8 +36,6 @@ class ListProviderAppointmentsService {
             });
 
             await this.cacheProvider.save(cacheKey, appointments);
-
-            console.log('buscou')
         }
 
         return appointments;
