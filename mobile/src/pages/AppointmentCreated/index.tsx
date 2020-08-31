f6104c6dfa6c81933a1864547f3c4e93e401c742
Lucas César <lucasambrogi1@hotmail.com>
2020-08-31 10:42:58 -0300

    Created AppointmentCreated page.

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import { Container, Title, Description, OkButton, OkButtonText } from './styles';
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [
+        { name: 'Dashboard' }
+      ],
+      index: 0,
+    });
+  }, [reset]);
+
   return (
     <Container>
-      <Text>AppointmentCreated</Text>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído!</Title>
+      <Description>Terça, dia 14 de março de 2020 às 12:00h</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
     </Container>
   );
 };
