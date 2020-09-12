2fe9afdb8cc1e746055f5c23ac570c8662c8234f
Lucas César <lucasambrogi1@hotmail.com>
2020-09-11 21:20:30 -0300

    Fixed some bugs on the splash screen

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,14 +1,19 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import AppProvider from './hooks';
 
 import Routes from './routes';
+import SplashScreen from 'react-native-splash-screen';
 
 const App: React.FC = () => {
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
+
     return (
         <NavigationContainer>
             <StatusBar barStyle="light-content" backgroundColor="#312e38" translucent />
